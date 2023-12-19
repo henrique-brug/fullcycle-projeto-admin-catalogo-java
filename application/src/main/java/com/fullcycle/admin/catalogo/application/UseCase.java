fadbd4191ee6c6d77140457c53bcf4c2820da15e
Henrique Lisboa Brugnarotto <henriquebrug@outlook.com>
2023-12-19 14:33:09 -0300

    Implementando o caso de uso de criação de categoria

--- application/src/main/java/com/fullcycle/admin/catalogo/application/UseCase.java
+++ application/src/main/java/com/fullcycle/admin/catalogo/application/UseCase.java
@@ -2,9 +2,6 @@ package com.fullcycle.admin.catalogo.application;
 
 import com.fullcycle.admin.catalogo.domain.category.Category;
 
-public class UseCase {
-    public Category execute() {
-        return null;
-       // return new Category();
-    };
+public abstract class UseCase<IN, OUT> {
+    public abstract OUT execute(IN anIn);
 }
