77972d01f7efcf4fc13fb6623b54aa5161c5282c
Henrique Lisboa Brugnarotto <henriquebrug@outlook.com>
2024-03-06 16:40:58 -0300

    Criando a estrategia para testes integrados de caso de uso

--- infrastructure/src/test/java/com/fullcycle/admin/catalogo/IntegrationTest.java
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/IntegrationTest.java
@@ -1,27 +1,21 @@
 package com.fullcycle.admin.catalogo;
 
-import org.junit.jupiter.api.extension.BeforeEachCallback;
+import com.fullcycle.admin.catalogo.infrastructure.configuration.WebServerConfig;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.extension.ExtensionContext;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.FilterType;
-import org.springframework.data.repository.CrudRepository;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import java.lang.annotation.*;
-import java.util.Collection;
 
 @Target(ElementType.TYPE)
 @Retention(RetentionPolicy.RUNTIME)
 @Inherited
 @ActiveProfiles("test")
-@DataJpaTest
-@ComponentScan(includeFilters = {
-        @ComponentScan.Filter(type = FilterType.REGEX, pattern = ".*[MySQLGateway]")
-})
+@SpringBootTest(classes = WebServerConfig.class)
 @ExtendWith(CleanUpExtension.class)
-public @interface MySQLGatewayTest {
+public @interface IntegrationTest {
 
 }
