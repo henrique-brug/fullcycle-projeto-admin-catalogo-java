f9d40ec0b451e74325f7916d3f52d4b227a73909
Henrique Lisboa Brugnarotto <henriquebrug@outlook.com>
2024-03-11 16:51:03 -0300

    Testes integrados do caso de uso de delete de categoria

---
+++ infrastructure/src/test/java/com/fullcycle/admin/catalogo/application/category/delete/DeleteCategoryUseCaseIT.java
@@ -0,0 +1,78 @@
+package com.fullcycle.admin.catalogo.application.category.delete;
+
+import com.fullcycle.admin.catalogo.IntegrationTest;
+import com.fullcycle.admin.catalogo.domain.category.Category;
+import com.fullcycle.admin.catalogo.domain.category.CategoryGateway;
+import com.fullcycle.admin.catalogo.domain.category.CategoryID;
+import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryJpaEntity;
+import com.fullcycle.admin.catalogo.infrastructure.category.persistence.CategoryRepository;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+
+import java.util.Arrays;
+
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+@IntegrationTest
+public class DeleteCategoryUseCaseIT {
+
+    @Autowired
+    private DeleteCategoryUseCase useCase;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @SpyBean
+    private CategoryGateway categoryGateway;
+
+    @Test
+    public void givenAValidId_whenCallsDeleteCategory_shouldBeOK() {
+        final var aCategory =
+                Category.newCategory("Filmes", "A categoria mais assistida", true);
+        final var expectedId = aCategory.getId();
+
+        save(aCategory);
+
+        Assertions.assertEquals(1, categoryRepository.count());
+
+        Assertions.assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        Assertions.assertEquals(0, categoryRepository.count());
+    }
+
+    @Test
+    public void givenAInvalidId_whenCallsDeleteCategory_shouldBeOK() {
+        final var expectedId = CategoryID.from("123");
+
+        Assertions.assertEquals(0, categoryRepository.count());
+
+        Assertions.assertDoesNotThrow(() -> useCase.execute(expectedId.getValue()));
+
+        Assertions.assertEquals(0, categoryRepository.count());
+    }
+
+    @Test
+    public void givenAValidId_whenGatewayThrowsExeption_shouldReturnException() {
+        final var aCategory =
+                Category.newCategory("Filmes", "A categoria mais assistida", true);
+        final var expectedId = aCategory.getId();
+
+        // Não faça nada quando eu chamar o deleteById[
+        doThrow(new IllegalStateException("Gateway error")).when(categoryGateway).deleteById(eq(expectedId));
+
+        Assertions.assertThrows(IllegalStateException.class, () -> useCase.execute(expectedId.getValue()));
+
+        verify(categoryGateway, times(1)).deleteById(expectedId);
+    }
+
+    private void save(final Category... aCategory) {
+        categoryRepository.saveAllAndFlush(
+                Arrays.stream(aCategory)
+                        .map(CategoryJpaEntity::from)
+                        .toList()
+        );
+    }
+}
